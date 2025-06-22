4d78b5973706804770b4a7664376168171cb0590
MichaelMortlockChapman <mmortlockchapman@gmail.com>
2025-06-23 00:34:06 +1000

    moved project folder and moved to WASM for auth connnection

---
+++ SimpleOnlineStore_DOTNET_BlazorWASM/Identity/Models/FormResult.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleOnlineStore_DOTNET_BlazorWASM.Identity.Models {
+    /// <summary>
+    /// Response for login and registration.
+    /// </summary>
+    public class FormResult {
+        /// <summary>
+        /// Gets or sets a value indicating whether the action was successful.
+        /// </summary>
+        public bool Succeeded { get; set; }
+
+        /// <summary>
+        /// On failure, the problem details are parsed and returned in this array.
+        /// </summary>
+        public string[] ErrorList { get; set; } = [];
+    }
+}
